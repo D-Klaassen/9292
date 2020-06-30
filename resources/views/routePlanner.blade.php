e079363da9002c2c090c384f1fb014ad4e72ec3b
Dion <D.B.A.Klaassen@gmail.com>
2020-06-30 20:21:12 +0200

    added total price check if exist and direction of train

--- resources/views/routePlanner.blade.php
+++ resources/views/routePlanner.blade.php
@@ -13,14 +13,18 @@
                 @foreach($route['transfers'] as $allTransfers)
                     <h2>vertrekken vanaf </h2>
                     <p>{{ $allTransfers['origin']['name'] }}</p>
-                    <h2>uitstappen op: </h2>
-                    <p>{{ $allTransfers['destination']['name'] }}</p>
+                    @if(array_key_exists('direction', $allTransfers))
+                        <h2>trein richting</h2>
+                        <p>{{ $allTransfers['direction'] }}</p>
+                    @endif
                     <h2>geplande vertrektijd trein: </h2>
                     <p>{{ date('d M Y H:i', strtotime($allTransfers['origin']['plannedDateTime'])) }}</p>
                     <h2>geplande aankomst:</h2>
                     <p>{{ date('d M Y H:i' , strtotime($allTransfers['destination']['plannedDateTime'])) }}</p>
+                    <h2>uitstappen op: </h2>
+                    <p>{{ $allTransfers['destination']['name'] }}</p>
                 @endforeach
-                @if(array_key_exists($route['totalPrice'], $route))
+                @if(array_key_exists('totalPrice', $route))
                     <h2>prijs van de reis</h2>
                     <p>&#8364;{{ $route['totalPrice'] }}</p>
                 @endif
